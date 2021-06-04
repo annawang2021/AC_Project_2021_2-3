1cad20c0382cd52c25925b710fbff677ee067a78
annawang2021 <juice830421@gmail.com>
2021-06-04 22:35:06 +0800

    Upload the whole project

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({defaultLayout:'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+const restaurantsList = require('./restaurant.json')
+
+
+app.get('/', (req, res) =>{
+    
+    res.render('index', {restaurants: restaurantsList.results})
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+    const restaurant = restaurantsList.results.find(item => item.id.toString() === req.params.restaurant_id)  
+    res.render('show', {restaurant: restaurant})
+  })
+
+app.get('/search', (req, res) => {
+    const keyword = req.query.keyword
+    const restaurants = restaurantsList.results.filter(item => {
+        return item.name.toLowerCase().includes(keyword.toLowerCase())
+    })
+    console.log(restaurants)
+    res.render('index', {restaurants: restaurants, keyword: keyword })
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Express is listening on localhost:${port}`)
+})
